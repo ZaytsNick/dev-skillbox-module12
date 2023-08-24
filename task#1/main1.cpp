ba8b956002ab54eb4eb0f4f88061d0be313b7912
Zayts Work <zaytsev@un74.ru>
2023-08-24 15:43:53 +0500

    task#1 complite

--- task#1/main1.cpp
+++ task#1/main1.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <string>
 int main()
 {
-    
+    std::string tenants[10];
+    int number_apartment;
+    for (int i = 0; i < 10; ++i)
+    {
+        std::cout << "Enter the surname of the tenant from apartment " << i + 1 << ": ";
+        std::cin >> tenants[i];
+    }
+
+    for (int j = 0; j < 3; ++j)
+    {
+        std::cout <<"\n Enter number apartment :";
+        std::cin >> number_apartment;
+        if (number_apartment > 0 && number_apartment < 11)
+        {
+            std::cout << "\nIn apartment number " << number_apartment << " resides " << tenants[number_apartment - 1];
+        }
+        else
+        {
+            std::cout << "You entered an invalid apartment number " << std::endl;
+            j--;
+        }
+    }
 }
