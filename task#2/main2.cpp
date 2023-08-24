b0732d6c0714611d6556eafa86be3b758040abbf
Zayts Work <zaytsev@un74.ru>
2023-08-24 15:45:55 +0500

    begin task#2

---
+++ task#2/main2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+int main()
+{
+    float enter[15],conclusion[15];
+    for (int i = 0; i < 15; ++i)
+    {
+        std::cout << "Enter fractional number" << i + 1 << ": ";
+        std::cin >> enter[i];
+    }
+    
+}
