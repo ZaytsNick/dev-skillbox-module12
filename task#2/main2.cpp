802e29dab8d20bdd7fb7cdce21bb5ad56c4ad7fe
Zayts Work <zaytsev@un74.ru>
2023-08-25 14:57:13 +0500

    task#2 complite

--- task#2/main2.cpp
+++ task#2/main2.cpp
@@ -2,18 +2,24 @@
 #include <string>
 int main()
 {
-    float enter[15], conclusion[15];
+    float enter[15];
     for (int i = 0; i < 15; ++i)
     {
-        std::cout << "Enter fractional number" << i + 1 << ": ";
+        std::cout << "\nEnter fractional number" << i + 1 << ": ";
         std::cin >> enter[i];
     }
-    conclusion[0]=enter[0];
-    for (int i = 0; i < 15; ++i)
+    std::cout<<"\n\n\n\n";
+    for (int j = 0; j < 15; ++j)
     {
-        for (int j = 0; j < 15; ++j)
+            int difference(j);
+        for(int x=j;x<15;++x)
         {
-            
+            if(enter[j]-enter[x]<=enter[j]-enter[difference])
+            {
+                difference=x;
+            }
         }
+        std::swap(enter[j],enter[difference]);
+        std::cout<<enter[j]<<" ";
     }
 }
